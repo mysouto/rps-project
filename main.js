230ac88b97b5a65fea2c1f975e81bd6fb3acf1d4
Milena <65979426+mysouto@users.noreply.github.com>
2022-06-08 15:36:23 -0700

    change variables to let and edit spelling and quotes

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 let playerSelection = window.prompt('Rock, Paper, or Scissors? ');
-const computerSelection = computerPlay();
+let computerSelection = computerPlay();
 
-// play against computer
+// returns random computer choice
 function computerPlay() {
-    // randomly return 'Rock', 'Paper', 'Scissors'
-    const choices = ['Rock', 'Paper', 'Scissors']
-    const rpsChoice = choices[Math.floor(Math.random() * choices.length)]
+    const choices = ['rock', 'paper', 'scissors']
+    let rpsChoice = choices[Math.floor(Math.random() * choices.length)]
     return rpsChoice
 }
 
 // function plays 1 round of rps
 function playRound(playerSelection, computerSelection) {
     // req: case-insensitive input for playerSelection
-    let player = playerSelection[0].toUpperCase() + playerSelection.substring(1)
+    // let player = playerSelection[0].toUpperCase() + playerSelection.substring(1)
+    let player = playerSelection.toLowerCase()
     let computer = computerSelection;
     if (player === computer) {
         return 'tie'
@@ -37,20 +37,20 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-// console.log(playRound(playerSelection, computerSelection));
+console.log(playRound(playerSelection, computerSelection));
 
 // function to play game 5 times 
 // output: winner in each round, final winner
-function game(n) {
-    for (let i = 0; i < n; i++) {
-        console.log(`Round ${i+1}`)
-        playRound(playerSelection, computerSelection)
+// function game(n) {
+//     for (let i = 0; i < n; i++) {
+//         console.log(`Round ${i+1}`)
+//         playRound(playerSelection, computerSelection)
         // if (playRound(playerSelection, computerSelection) == `You Win! ${player} beats ${computer}`) {
         //     playerWins += 1
         // } else if (playRound(playerSelection, computerSelection) == `You Lose! ${computer} beats ${player}`) {
         //     computerWins += 1
         // }
-    }
-}
+    // }
+// }
 
 // game(5)
